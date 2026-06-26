a9f7f0285c5a98f6b563b055d311069b57e6f111
fpcoutinho <fpcoutinho5@gmail.com>
2026-06-26 11:41:37 -0300

    core: add app structure (node, express, ts, docker)

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { app } from './app.ts'
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(
+    JSON.stringify({
+      message: 'Server is running',
+      port: PORT,
+    }),
+  )
+})
