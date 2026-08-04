b330768952cb4890250c0dc81e71291f689afeb4
Filipe Coutinho <fpcoutinho5@gmail.com>
2026-08-04 10:02:48 -0300

    feat: add user balance endpoint

---
+++ src/modules/user/get-user-balance.schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+import { baseSchema } from '../../shared/validation/base-schema.ts'
+
+export const schema = {
+  response: {
+    ...baseSchema.response,
+    body: z.object({
+      raw: z.string(),
+      formatted: z.string(),
+      currency: z.string()
+    })
+  }
+}
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { createUser } from './create-user.controller.ts'
+import { getUserBalance } from './get-user-balance.controller.ts'
 import { getUserProfile } from './get-user-profile.controller.ts'
 import { updateUserProfile } from './update-user-profile.controller.ts'
 
@@ -8,3 +9,4 @@ export const userRouter = Router()
 userRouter.post('/', createUser)
 userRouter.get('/', getUserProfile)
 userRouter.patch('/', updateUserProfile)
+userRouter.get('/balance', getUserBalance)
